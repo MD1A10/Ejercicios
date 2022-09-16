6a6df4c4c4e9c20d69f6b6dcd637c09a54094cf4
MD1A10 <113714164+MD1A10@users.noreply.github.com>
2022-09-16 11:23:04 -0600

    Add files via upload

---
+++ P2Unidad2Semana2Clase4Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2.unidad.pkg2semana.pkg2clase.pkg4ejercicio1;
+
+/**
+ *
+ * @author Monica
+ */
+public class P2Unidad2Semana2Clase4Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
